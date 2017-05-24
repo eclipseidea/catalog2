7a465f00e8035d3765ab2a80b553a208cf805d20
eclipseidea <natanid@yandex.ua>
2017-05-24 15:00:28 +0300

    [!]

--- src/main/java/zab/romik/dao/CountryDao.java
+++ src/main/java/zab/romik/dao/CountryDao.java
@@ -4,6 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import zab.romik.entity.Counrty;
 
 /**
+ * CountryDAO
+ *
  * @since 0.0.1
  */
 public interface CountryDao extends JpaRepository<Counrty, Integer> {
