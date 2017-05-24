efee8c4a007452aa5a752fd1b075882eac0913c0
natanid <natanid@yandex.ua>
2017-05-24 14:56:21 +0300

    + add since javadoc tag to CountryDAO

--- src/main/java/zab/romik/dao/CountryDao.java
+++ src/main/java/zab/romik/dao/CountryDao.java
@@ -3,6 +3,9 @@ package zab.romik.dao;
 import org.springframework.data.jpa.repository.JpaRepository;
 import zab.romik.entity.Counrty;
 
+/**
+ * @since 0.0.1
+ */
 public interface CountryDao extends JpaRepository<Counrty, Integer> {
 //	Counrty  findByName(String name);
 //    @Query("select  r from Country r left join fetch r.commodity where r.id=:id")
