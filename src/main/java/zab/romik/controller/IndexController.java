e230fd89494f0a2d181badb42a4cc1d0b1ab8e47
ROMIK <pelipec1980@gmail.com>
2017-05-24 19:42:42 +0300

    new javadoc

--- src/main/java/zab/romik/controller/IndexController.java
+++ src/main/java/zab/romik/controller/IndexController.java
@@ -2,7 +2,9 @@ package zab.romik.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
+/*
+* 0.0.1
+* */
 @Controller
 public class IndexController {
     @GetMapping("/")
