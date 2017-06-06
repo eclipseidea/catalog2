e761ad58abf4126afed1fc906e31ab42a60cf479
Vjachelsav Gusser <proweber1@yandex.ru>
2017-06-06 22:38:02 +0300

    load commodity categories to view

--- src/main/java/zab/romik/controller/CommodityController.java
+++ src/main/java/zab/romik/controller/CommodityController.java
@@ -8,38 +8,51 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import zab.romik.entity.Commodity;
+import zab.romik.service.CategoriesService;
 import zab.romik.service.CommodityService;
 
-/**
- * Created by ROMIK on 24.05.2017.
- */
 @Controller
 public class CommodityController {
 
-    @Autowired
     private CommodityService commodityService;
+    private final CategoriesService categoriesService;
 
+    @Autowired
+    public CommodityController(final CommodityService commodityService,
+                               final CategoriesService categoriesService) {
+        this.commodityService = commodityService;
+        this.categoriesService = categoriesService;
+    }
+
+    /**
+     * Страница со списком товаров.
+     *
+     * @param model Свойства которые будут инжектиться в отображение
+     * @return Имя вьюхи которую надо отрендерить
+     */
     @GetMapping("/commodity")
-    public String Commodity(Model model){
-        model.addAttribute("commodities",commodityService.findAll());
+    public String Commodity(Model model) {
+        model.addAttribute("commodities", commodityService.findAll());
         model.addAttribute("commodity", new Commodity());
+        model.addAttribute("categories", categoriesService.findAll());
+
         return "commodity";
     }
 
     @PostMapping("/commodity")
-    public String Commodity(@ModelAttribute Commodity commodity){
+    public String Commodity(@ModelAttribute Commodity commodity) {
         commodityService.save(commodity);
         return "redirect:/commodity";
     }
 
     @GetMapping("/deleteCommodity/{id}")
-    public String delete(@PathVariable long id){
+    public String delete(@PathVariable long id) {
         commodityService.delete(id);
         return "redirect:/commodity";
     }
 
     @GetMapping("/updateCommodity/{id}")
-    public String update(@ModelAttribute Commodity commodity){
+    public String update(@ModelAttribute Commodity commodity) {
         commodityService.update(commodity);
         return "redirect:/commodity";
     }
